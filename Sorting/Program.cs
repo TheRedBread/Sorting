0907bab17191f1230beec12c6f3bffe1e35706c0
TheRedBread <159266466+TheRedBread@users.noreply.github.com>
2025-09-14 15:49:59 +0200

    Added array and display

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 
-namespace sort
+class Sorting
 {
-    class Program
+
+
+
+    static void DisplayArray(int[] arr)
     {
-        static void Main(string[] args)
+        Console.Write("[");
+        for (int i = 0; i < arr.Length; i++)
         {
+            Console.Write(arr[i]);
+            if (i == arr.Length-1) continue;
+            Console.Write(",");
         }
+        Console.Write("]");
     }
+
+
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 3, 65, 1, 87, 57, 26, 189, 34, 12, 75, 23 };
+        Console.WriteLine("Array Length: " + arr.Length);
+        DisplayArray(arr);
+
+    }
+
+
+
+
 }
