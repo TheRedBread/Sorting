ae6006bf3b80835c200fbb5c3485e68ff9aca84e
TheRedBread <159266466+TheRedBread@users.noreply.github.com>
2025-09-14 16:39:04 +0200

    Random Array Generation

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -30,11 +30,14 @@ class Sorting
     {
         int temp;
         for (int i = 0; i < arr.Length-1; i++)
-        {
+        { 
+            // selects i
             for (int j = 0; j < arr.Length-i-1; j++)
-            {
+            { 
+                // selects and compares to other numbers
                 if (arr[j] > arr[j + 1])
-                {
+                { 
+                    // switches places if necesary
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -42,16 +45,25 @@ class Sorting
             }
 
         }
-
-
     }
 
+    // Generates a list of length of n and values from [min, max]
+    static int[] RandomArray(int n, int min, int max)
+    {
+        int[] arr = new int[n];
+        Random r = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = r.Next(min, max);
+        }
+        return arr;
+    }
 
 
     static void Main(string[] args)
     {
-        int[] arr = { 3, 65, 1, 87, 57, 26, 189, 34, 12, 75, 23 };
-        Console.WriteLine("Array Length: " + arr.Length);
+        int[] arr = RandomArray(10, 0, 10);
+        
         PrintArray(arr);
         BubbleSort(arr);
         PrintArray(arr);
