9d7b6308dcf153b68a5c791a78e1b767f5d2ffc4
TheRedBread <159266466+TheRedBread@users.noreply.github.com>
2025-09-17 08:35:12 +0200

    Merge sort

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Sorting
 {
@@ -78,14 +79,80 @@ class Sorting
             int key = arr[i];
             int j = i - 1;
 
+            // move everything ahead
             while (j >= 0 && key < arr[j])
             {
                 arr[j + 1] = arr[j];
                 j -= 1;
             }
+            // put the key at the correct position
             arr[j + 1] = key;
 
         }
+    }
+
+
+    static void merge(int[] arr, int left, int mid, int right)
+    {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        int i, j;
+
+        for (i = 0; i < n1; i++) L[i] = arr[left + i];
+        for (j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+        i = 0;
+        j = 0;
+
+        int k = left;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+
+
+    }
+
+    static void MergeSort(int[] arr, int left, int right)
+    {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        MergeSort(arr, left, mid);
+        MergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+
+
     }
 
 
@@ -108,7 +175,7 @@ class Sorting
         int[] arr = RandomArray(10, 0, 10);
         
         PrintArray(arr);
-        InsertionSort(arr);
+        MergeSort(arr, 0, arr.Length - 1);
         PrintArray(arr);
 
 
